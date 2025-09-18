d95bf4a4b0b6c386c9738cf9622a89989b74da39
liuyuweitarek <liuyuwei.tarek@gmail.com>
2025-09-18 18:08:01 +0800

    docs: apply Pytorch sphinx theme

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Tversky-Neural-Network-PyTorch'
+copyright = '2025, Tarek Liu'
+author = 'Tarek Liu'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+html_theme = 'pytorch_sphinx_theme'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
---
+++ docs/source/index.rst
@@ -0,0 +1,10 @@
+.. Tversky-Neural-Network-PyTorch documentation master file, created by
+   sphinx-quickstart on Fri Sep  5 03:03:15 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Tversky-Neural-Network-PyTorch
+==============================
+
+.. automodule:: tversky_neural_network.tversky_neural_network
+   :members:
